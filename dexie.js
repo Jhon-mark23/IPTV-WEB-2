5e0854c4368a253c49b94741ea9c5a554c35ef5d
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-07-07 23:57:17 +0800

    Update dexie.js

--- dexie.js
+++ dexie.js
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 
 // Create database and playlist store/collection
-const db = new Dexie("IPTV");
+const db = new Dexie("https://github.com/Jhon-mark23/PH/blob/main/PH%20LINK%201");
 db.version(1).stores({
   playlists: "++id,&name,data",
 });
